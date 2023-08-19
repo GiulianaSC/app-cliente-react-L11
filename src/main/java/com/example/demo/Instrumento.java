6165e103a9356318e26b2c0c1c7b69194b8a8ac4
Giuliana Sullca <114102411+GiulianaSC@users.noreply.github.com>
2023-08-19 05:13:00 +0000

    Backend y frontend funcionando con entidad

--- src/main/java/com/example/demo/Instrumento.java
+++ src/main/java/com/example/demo/Instrumento.java
@@ -33,6 +33,38 @@ public class Instrumento {
 			Objects.equals(descripcion, employee.descripcion);
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
 	@Override
 	public int hashCode() {
 
